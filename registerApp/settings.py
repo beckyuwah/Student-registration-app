b93dccc3f4840f7723ebf7fd92d011ffb7bfff19
Becky Uwah <beckyuwah@Beckys-MacBook-Pro.local>
2025-09-15 12:34:55 +0100

    update db setting

--- registerApp/settings.py
+++ registerApp/settings.py
@@ -111,8 +111,15 @@ WSGI_APPLICATION = 'registerApp.wsgi.application'
 #CLOSE-FOR-DEPLOYMENT
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',         # Switch backend to MySQL
+        'NAME': 'django_db',                          # DB name (from docker-compose.yml)
+        'USER': 'django',                             # DB user (from docker-compose.yml)
+        'PASSWORD': 'django',                         # DB password (from docker-compose.yml)
+        'HOST': 'db',                                 # Service name in docker-compose
+        'PORT': '3306',                               # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
